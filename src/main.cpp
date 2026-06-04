80f035fe0547959bd11259dc69613872a5175e7d
CoderEx24 <coderex24@protonmail.com>
2026-06-04 14:08:24 +0300

    cleanup main to use arrays of stuff instead of calling static functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
-#include <vector>
-#include <string>
+#include <array>
+#include <memory>
 
 #include <raylib.h>
 #define RAYGUI_IMPLEMENTATION
 #include <raygui.h>
 #include <raygui_styles/cyber/style_cyber.h>
 
-#include "visualisation.hpp"
+#include "dummy.hpp"
+#include "heap.hpp"
 
 #define WINDOW_WIDTH 1200
 #define WINDOW_HEIGHT 800
@@ -20,11 +21,19 @@ int main()
 
     int scroll_index = 0;
     int active = 0;
-    int current_choice = -1;
+    int current_choice = 0;
 
-    std::vector<std::string> algorithms_list = Visualisation::get_visualisations_list();
-    std::string algorithms_list_string = Visualisation::get_visualisations_list_string();
-    std::unique_ptr<Visualisation> vis = nullptr;
+    std::array<std::unique_ptr<Visualisation>, 2> algorithms_list { 
+        std::make_unique<DummyVisualisation>(),
+        std::make_unique<HeapVisualisation>(),
+    };
+
+    std::array algorithms_labels {
+        "Dummy",
+        "Heap"
+    };
+
+    const char* algorithms_list_string = "Dummy;Heap\0" ;
 
     while(!WindowShouldClose())
     {
@@ -37,23 +46,12 @@ int main()
 
         const int LIST_WIDTH = WINDOW_WIDTH * 0.20;
         const int LIST_HEIGHT = WINDOW_HEIGHT;
-        GuiListView({ 0, 0, LIST_WIDTH, LIST_HEIGHT }, algorithms_list_string.c_str(), &scroll_index, &active);
+        GuiListView({ 0, 0, LIST_WIDTH, LIST_HEIGHT }, algorithms_list_string, &scroll_index, &active);
 
 
-        if (active >= 0 && current_choice != active)
-        {
-            const char* chosen_algo = algorithms_list[active].c_str();
-
-            vis = Visualisation::get_visualisation(chosen_algo);
-
-            current_choice = active;
-        }
-        else if (active == -1)
-            vis = nullptr;
-
-        GuiLabel({ LIST_WIDTH * 1.02, 10, WINDOW_WIDTH - LIST_WIDTH, 30 }, algorithms_list[current_choice].c_str());
+        GuiLabel({ LIST_WIDTH * 1.02, 10, WINDOW_WIDTH - LIST_WIDTH, 30 }, algorithms_labels[current_choice]);
         
-        if (vis)
+        if (current_choice > -1)
         {
             Rectangle visualisation_bounds = {LIST_WIDTH * 1.02,
                                               WINDOW_HEIGHT * 0.10, 
@@ -66,9 +64,9 @@ int main()
                                         (WINDOW_WIDTH - LIST_WIDTH) * 0.4,
                                         WINDOW_HEIGHT * 0.8};
 
-            vis->paint_visualisation(visualisation_bounds, GetFrameTime());
-            GuiWindowBox(control_bounds, algorithms_list[current_choice].c_str());
-            vis->paint_controls(control_bounds);
+            algorithms_list[current_choice]->paint_visualisation(visualisation_bounds, GetFrameTime());
+            GuiWindowBox(control_bounds, algorithms_labels[current_choice]);
+            algorithms_list[current_choice]->paint_controls(control_bounds);
         }
         EndDrawing();
     }
