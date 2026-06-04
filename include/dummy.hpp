7604764895705f072c25b11982cfdeeedb7c6f2d
CoderEx24 <coderex24@protonmail.com>
2026-06-04 13:45:27 +0300

    separate the dummy visualisation into its own files

---
+++ include/dummy.hpp
@@ -0,0 +1,21 @@
+#ifndef DUMMY_H
+#define DUMMY_H
+
+#include "visualisation.hpp"
+#include <string>
+#include <raygui.h>
+
+class DummyVisualisation : public Visualisation
+{
+    private:
+        std::string str;
+        bool enable_textbox = false;
+    public:
+        void paint_visualisation(Rectangle bounds, float dt) override;
+        void paint_controls(Rectangle bounds) override;
+        
+};
+
+
+#endif
+
---
+++ src/dummy.cpp
@@ -0,0 +1,19 @@
+#include "dummy.hpp"
+
+void DummyVisualisation::paint_visualisation(Rectangle bounds, float dt)
+{
+    DrawRectangle(bounds.x, bounds.y, bounds.width, bounds.height, 
+            WHITE);
+    DrawText("This is a dummy visualisation\nChoose something from\nthe list on the left",
+            bounds.x + 12, bounds.y + 12, 20, BLACK);
+
+    DrawText(TextFormat("Entered: %s", str.c_str()), bounds.x * 1.01f, bounds.y * 2.50f, 20, RED); 
+}
+
+void DummyVisualisation::paint_controls(Rectangle bounds)
+{
+    GuiToggle({ bounds.x * 1.30f, bounds.y * 1.40f,
+                80, 30 }, "Enable", &enable_textbox);
+    GuiTextBox({ bounds.x * 1.01f, bounds.y * 1.40f,
+                 bounds.width * 0.60f, 30}, str.data(), 30, enable_textbox);
+}
